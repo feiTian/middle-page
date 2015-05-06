95ca40f6d3cd7e95544b206a4526c4c0ff6a7e1a
feiTian <hero_afei@163.com>
2015-05-06 15:32:20 +0800

    add post AdRequest

--- app.js
+++ app.js
@@ -10,31 +10,18 @@ var app = express()
 
 // Setup header, for initial debugging
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('X-Powered-By','3.2.1');
+    res.header('Content-Type', 'application/json;charset=utf-8');
     next();
 }
 
-app.use(allowCrossDomain);
+app.all(allowCrossDomain);
 app.use('/', express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-//*****user routes
-app.post('/user', user.doLogin);
-app.post('/code', user.fetchCode_res);
-
-//*****user request routes
-app.post('/userRequest', userRequest.createUserRequest);
-app.get( '/userRequest/:id/offer', userRequest.getOfferList);
-app.post('/userRequest/:id/offerTaken', userRequest.takeOffer);
-app.post('/userRequest/:id/offerConfirmation', userRequest.confirmOffer);
-
-//*****restaurant routes
-app.get('/restaurant', restaurant.getNearbyRestaurant);
-app.get('/restaurant/:restaurantID/menu', restaurant.getMenuList);
-app.post('/restaurant/register', restaurant.register);
 
 var server = app.listen(9000, function () {
 
