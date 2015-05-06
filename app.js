535b8e16fdfa3df25e5ac01a6e5c3117e2c46d8f
feiTian <hero_afei@163.com>
2015-05-06 15:46:18 +0800

    change

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ var app = express()
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('X-Powered-By','3.2.1');
-    res.header('Content-Type', 'application/json;charset=utf-8');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
     next();
 }
 
